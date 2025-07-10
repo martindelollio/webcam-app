f04ddfc593c53ca9b7b6a5a073cbfaaee8063748
Martin Del Ollio <110709751+martindelollio@users.noreply.github.com>
2025-07-10 17:39:06 -0300

    agregado de funciones para analizar fotos

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def list_camera():
 
 
 def capture_image(camera_index):
-    cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
+    cap = cv2.VideoCapture(camera_index, cv2.CAP_MSMF)
     time.sleep(0.5)
     if not cap.isOpened():
         print("No se pudo abirir la camara")
@@ -33,12 +33,49 @@ def capture_image(camera_index):
     ret, frame = cap.read()
     cap.release()
     if ret:
-        filename = 'static/captured_image.jpg'
+        
+        filename = 'webcam-app/static/captured_image.jpg'
         cv2.imwrite(filename,frame)
+        print(f'Imagen guardada como {filename}')
         return filename
         
     else:
         print('error al imprimir la imagen')
 
-print(list_camera())
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+def analizar_foto(image_path):
+    from openai import OpenAI
+    base64_image = encode_image(image_path)
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Describe la imagen en detalle"},
+                    {
+                        "type": "image_url",
+                        "image_url":{
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                            "detail": "high"
+
+                        }
+                    }
+                ]
+            }
+        ],
+        max_tokens=300,
+    )
+
+
+    return response.choices[0].message.content
+
+
 capture_image(0)
+respuesta = analizar_foto("webcam-app/static/captured_image.jpg")
+print(respuesta)
